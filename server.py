184b6bc5024f4a4f9255c86c6e9aaa99fa4351a3
Fabian Jahr <fabian.jahr@whu.edu>
2019-02-02 00:25:47 +0100

    Fix requests bug

--- server.py
+++ server.py
@@ -1,12 +1,13 @@
 import config
 import requests
+import json
 
 def new_invoice(amount_mbtc):
     url = config.charge_url + '/invoice'
     amount_msat = amount_mbtc/100000000
     req_data = {'msatoshi': str(amount_msat), 'metadata': {'description': 'lightningvendor.com'}}
 
-    r = requests.post(url, data = req_data).json()
+    r = requests.post(url, data=json.dumps(req_data)).json()
 
     return r['id'], r['payreq']
 
