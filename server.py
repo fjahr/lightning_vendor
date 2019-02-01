9c1153a0010fde2b2e9a36e2378c6457e0ba1e3f
Fabian Jahr <fabian.jahr@whu.edu>
2019-02-01 23:19:46 +0100

    Parse response json

--- server.py
+++ server.py
@@ -6,14 +6,14 @@ def new_invoice(amount_mbtc):
     amount_msat = amount_mbtc/100000000
     req_data = {'msatoshi': str(amount_msat), 'metadata': {'description': 'lightningvendor.com'}}
 
-    r = requests.post(url, data = req_data)
+    r = requests.post(url, data = req_data).json()
 
     return r['id'], r['payreq']
 
 def invoice_paid(id):
     url = config.charge_url + '/invoice/' + id
 
-    r = requests.get(url)
+    r = requests.get(url).json()
 
     if r['status'] == 'paid':
         return True
