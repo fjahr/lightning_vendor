6e201b08e6d43c5e1facbd26d6db9bac060797a6
Fabian Jahr <fabian.jahr@whu.edu>
2019-03-06 21:27:04 +0000

    improve loop functionality

--- vendor.py
+++ vendor.py
@@ -4,7 +4,6 @@ from price import Price, PriceUpdater
 from display import Display
 from keypad import keypad
 import time
-import RPi.GPIO as GPIO
 
 class Vendor:
     def __init__(self):
@@ -15,52 +14,45 @@ class Vendor:
         PriceUpdater(self)
 
     def start(self):
-        try:
-            self.state = "welcome"
-            self.cleanup()
-            self.display.welcome(self.price)
-            self.keypad.registerKeyPressHandler(self.selection)
+        self.state = "welcome"
+        self.cleanup()
+        self.display.welcome(self.price)
+        self.keypad.registerKeyPressHandler(self.selection)
 
-            while True:
-                time.sleep(0.1)
-        finally:
-            GPIO.cleanup()
+        while True:
+            time.sleep(0.1)
 
     def cleanup(self):
         self.display.clean_invoice()
 
     def selection(self, key):
-        try:
-            if key in ["1", "2", "3", "4"]:
-                self.state = "invoice"
-                self.display.choice(key)
+        if key in ["1", "2", "3", "4"]:
+            self.state = "invoice"
+            self.display.choice(key)
 
-                id, invoice = new_invoice(self.price)
-                self.display.invoice(invoice)
-                self.keypad.registerKeyPressHandler(self.cancel)
+            id, invoice = new_invoice(self.price)
+            self.display.invoice(invoice)
+            self.keypad.registerKeyPressHandler(self.cancel)
 
-                counter = 0
-                while True:
-                    time.sleep(1)
-                    counter += 1
+            counter = 0
+            while True:
+                time.sleep(1)
+                counter += 1
 
+                # TODO
+                print("waiting for payment try " + str(counter))
+                print("checking invoice " + str(id))
+
+                if invoice_paid(id) or (counter > 10):
+                    self.display.thank()
                     # TODO
-                    print("waiting for payment try " + str(counter))
-                    print("checking invoice " + str(id))
-
-                    if invoice_paid(id) or (counter > 10):
-                        self.display.thank()
-                        # TODO
-                        print("GIVE CANDY!!!!!!!!!!!")
-                        Servo(key).release()
-                        self.start()
-                    elif counter > 60:
-                        self.start()
+                    print("GIVE CANDY!!!!!!!!!!!")
+                    Servo(key).release()
                     break
-            else:
-                self.start()
-        finally:
-            GPIO.cleanup()
+                elif counter > 60:
+                    break
+
+        self.start()
 
     def cancel(self, key):
         self.start()
